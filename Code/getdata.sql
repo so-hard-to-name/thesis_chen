21167a10fd3ff2de4c3288f4574e201121237c9d
chen <chen.finland@outlook.com>
2023-05-14 13:42:47 +0300

    add lods to sql

--- Code/getdata.sql
+++ Code/getdata.sql
@@ -15,7 +15,8 @@ WITH demographic AS (
     LEFT JOIN `physionet-data.mimiciv_hosp.patients` pa
         ON ie.subject_id = pa.subject_id
     WHERE pa.anchor_age + DATETIME_DIFF(ie.intime, DATETIME(pa.anchor_year, 1, 1, 0, 0, 0), YEAR) > 18
-    )
+)
+
 , vitalsign AS (
     SELECT
     ie.subject_id
@@ -72,7 +73,8 @@ WITH demographic AS (
         ON ie.stay_id = g.stay_id
             AND g.charttime >= DATETIME_SUB(ie.intime, INTERVAL '6' HOUR)
             AND g.charttime <= DATETIME_ADD(ie.intime, INTERVAL '1' DAY)
-    )
+)
+
 , tm AS (
     SELECT stay_id, charttime
     FROM `physionet-data.mimiciv_derived.ventilator_setting`
@@ -296,9 +298,193 @@ WITH demographic AS (
     -- for efficiency, we use an aggregate here,
     -- but we could equally well group by this column
     , MAX(ventilation_status) AS ventilation_status
-FROM vd2
-GROUP BY stay_id, vent_seq
-HAVING MIN(charttime) != MAX(charttime)
+    FROM vd2
+    GROUP BY stay_id, vent_seq
+    HAVING MIN(charttime) != MAX(charttime)
+)
+
+, cpap AS (
+    SELECT ie.stay_id
+        , MIN(DATETIME_SUB(charttime, INTERVAL '1' HOUR)) AS starttime
+        , MAX(DATETIME_ADD(charttime, INTERVAL '4' HOUR)) AS endtime
+        , MAX(CASE
+                WHEN LOWER(ce.value) LIKE '%cpap%' THEN 1
+                WHEN LOWER(ce.value) LIKE '%bipap mask%' THEN 1
+                ELSE 0 END) AS cpap
+    FROM `physionet-data.mimiciv_icu.icustays` ie
+    INNER JOIN `physionet-data.mimiciv_icu.chartevents` ce
+        ON ie.stay_id = ce.stay_id
+            AND ce.charttime >= ie.intime
+            AND ce.charttime <= DATETIME_ADD(ie.intime, INTERVAL '1' DAY)
+    WHERE itemid = 226732
+        AND (
+            LOWER(ce.value) LIKE '%cpap%' OR LOWER(ce.value) LIKE '%bipap mask%'
+        )
+    GROUP BY ie.stay_id
+)
+
+, pafi1 AS (
+    -- join blood gas to ventilation durations to determine if patient was vent
+    -- also join to cpap table for the same purpose
+    SELECT ie.stay_id, bg.charttime
+        , pao2fio2ratio
+        , CASE WHEN vd.stay_id IS NOT NULL THEN 1 ELSE 0 END AS vent
+        , CASE WHEN cp.stay_id IS NOT NULL THEN 1 ELSE 0 END AS cpap
+    FROM `physionet-data.mimiciv_derived.bg` bg
+    INNER JOIN `physionet-data.mimiciv_icu.icustays` ie
+        ON bg.hadm_id = ie.hadm_id
+            AND bg.charttime >= ie.intime AND bg.charttime < ie.outtime
+    LEFT JOIN `physionet-data.mimiciv_derived.ventilation` vd
+        ON ie.stay_id = vd.stay_id
+            AND bg.charttime >= vd.starttime
+            AND bg.charttime <= vd.endtime
+            AND vd.ventilation_status = 'InvasiveVent'
+    LEFT JOIN cpap cp
+        ON ie.stay_id = cp.stay_id
+            AND bg.charttime >= cp.starttime
+            AND bg.charttime <= cp.endtime
+)
+
+, pafi2 AS (
+    -- get the minimum PaO2/FiO2 ratio *only for ventilated/cpap patients*
+    SELECT stay_id
+        , MIN(pao2fio2ratio) AS pao2fio2_vent_min
+    FROM pafi1
+    WHERE vent = 1 OR cpap = 1
+    GROUP BY stay_id
+)
+
+, cohort AS (
+    SELECT ie.subject_id
+        , ie.hadm_id
+        , ie.stay_id
+        , ie.intime
+        , ie.outtime
+
+        , gcs.gcs_min
+        , vital.heart_rate_max
+        , vital.heart_rate_min
+        , vital.sbp_max
+        , vital.sbp_min
+
+        -- this value is non-null iff the patient is on vent/cpap
+        , pf.pao2fio2_vent_min
+
+        , labs.bun_max
+        , labs.bun_min
+        , labs.wbc_max
+        , labs.wbc_min
+        , labs.bilirubin_total_max AS bilirubin_max
+        , labs.creatinine_max
+        , labs.pt_min
+        , labs.pt_max
+        , labs.platelets_min AS platelet_min
+
+        , uo.urineoutput
+
+    FROM `physionet-data.mimiciv_icu.icustays` ie
+    INNER JOIN `physionet-data.mimiciv_hosp.admissions` adm
+        ON ie.hadm_id = adm.hadm_id
+    INNER JOIN `physionet-data.mimiciv_hosp.patients` pat
+        ON ie.subject_id = pat.subject_id
+
+    -- join to above view to get pao2/fio2 ratio
+    LEFT JOIN pafi2 pf
+        ON ie.stay_id = pf.stay_id
+
+    -- join to custom tables to get more data....
+    LEFT JOIN `physionet-data.mimiciv_derived.first_day_gcs` gcs
+        ON ie.stay_id = gcs.stay_id
+    LEFT JOIN `physionet-data.mimiciv_derived.first_day_vitalsign` vital
+        ON ie.stay_id = vital.stay_id
+    LEFT JOIN `physionet-data.mimiciv_derived.first_day_urine_output` uo
+        ON ie.stay_id = uo.stay_id
+    LEFT JOIN `physionet-data.mimiciv_derived.first_day_lab` labs
+        ON ie.stay_id = labs.stay_id
+)
+
+, scorecomp AS (
+    SELECT
+        cohort.*
+  -- Below code calculates the component scores needed for SAPS
+
+        -- neurologic
+        , CASE
+            WHEN gcs_min IS NULL THEN null
+            WHEN gcs_min < 3 THEN null -- erroneous value/on trach
+            WHEN gcs_min <= 5 THEN 5
+            WHEN gcs_min <= 8 THEN 3
+            WHEN gcs_min <= 13 THEN 1
+            ELSE 0
+        END AS neurologic
+
+        -- cardiovascular
+        , CASE
+            WHEN heart_rate_max IS NULL
+                AND sbp_min IS NULL THEN null
+            WHEN heart_rate_min < 30 THEN 5
+            WHEN sbp_min < 40 THEN 5
+            WHEN sbp_min < 70 THEN 3
+            WHEN sbp_max >= 270 THEN 3
+            WHEN heart_rate_max >= 140 THEN 1
+            WHEN sbp_max >= 240 THEN 1
+            WHEN sbp_min < 90 THEN 1
+            ELSE 0
+        END AS cardiovascular
+
+        -- renal
+        , CASE
+            WHEN bun_max IS NULL
+                OR urineoutput IS NULL
+                OR creatinine_max IS NULL
+                THEN null
+            WHEN urineoutput < 500.0 THEN 5
+            WHEN bun_max >= 56.0 THEN 5
+            WHEN creatinine_max >= 1.60 THEN 3
+            WHEN urineoutput < 750.0 THEN 3
+            WHEN bun_max >= 28.0 THEN 3
+            WHEN urineoutput >= 10000.0 THEN 3
+            WHEN creatinine_max >= 1.20 THEN 1
+            WHEN bun_max >= 17.0 THEN 1
+            WHEN bun_max >= 7.50 THEN 1
+            ELSE 0
+        END AS renal
+
+        -- pulmonary
+        , CASE
+            WHEN pao2fio2_vent_min IS NULL THEN 0
+            WHEN pao2fio2_vent_min >= 150 THEN 1
+            WHEN pao2fio2_vent_min < 150 THEN 3
+            ELSE null
+        END AS pulmonary
+
+        -- hematologic
+        , CASE
+            WHEN wbc_max IS NULL
+                AND platelet_min IS NULL
+                THEN null
+            WHEN wbc_min < 1.0 THEN 3
+            WHEN wbc_min < 2.5 THEN 1
+            WHEN platelet_min < 50.0 THEN 1
+            WHEN wbc_max >= 50.0 THEN 1
+            ELSE 0
+        END AS hematologic
+
+        -- hepatic
+        -- We have defined the "standard" PT as 12 seconds.
+        -- This is an assumption and subsequent analyses may be
+        -- affected by this assumption.
+        , CASE
+            WHEN pt_max IS NULL
+                AND bilirubin_max IS NULL
+                THEN null
+            WHEN bilirubin_max >= 2.0 THEN 1
+            WHEN pt_max > (12 + 3) THEN 1
+            WHEN pt_min < (12 * 0.25) THEN 1
+            ELSE 0
+        END AS hepatic
+
+    FROM cohort
 )
 
 SELECT 
@@ -337,6 +523,19 @@ SELECT
     , gcs.gcs_eyes
     , gcs.gcs_unable
     , vs_final.ventilation_status
+    , COALESCE(neurologic, 0)
+    + COALESCE(cardiovascular, 0)
+    + COALESCE(renal, 0)
+    + COALESCE(pulmonary, 0)
+    + COALESCE(hematologic, 0)
+    + COALESCE(hepatic, 0)
+    AS lods
+    , neurologic
+    , cardiovascular
+    , renal
+    , pulmonary
+    , hematologic
+    , hepatic
     FROM demographic
     LEFT JOIN vitalsign 
         ON demographic.stay_id = vitalsign.stay_id
@@ -347,3 +546,5 @@ SELECT
         ON demographic.stay_id = vs_final.stay_id
             AND vs_final.starttime >= DATETIME_SUB(demographic.intime, INTERVAL '6' HOUR)
             AND vs_final.starttime <= DATETIME_ADD(demographic.intime, INTERVAL '1' DAY)
+    LEFT JOIN scorecomp s
+          ON demographic.stay_id = s.stay_id
